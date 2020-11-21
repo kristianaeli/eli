85f3f56ca1ff1d1e5f9aec8aac80e6a554173435
kristianaeli <70437455+kristianaeli@users.noreply.github.com>
2020-11-22 02:29:12 +0700

    Update Kristianaeli.py

--- Kristianaeli.py
+++ Kristianaeli.py
@@ -1,123 +1,74 @@
+#ToolsByKhairulFatihin
+#MANTANPRAWAN.$
+#Boleh Record Asal Cantumkan Nama Author
+#Hargai Karya Saya :)
 
-#!/bin/bash
-#version 1.0
-
-#MAU NGAPAIN TOT?:V
-#RECODE?REEDIT?RECOPYRIGHT?COPAS?
-#MALU TOLOL!!!
-#KAGA BERMORAL!
+import os,sys,time
 
 
-#01/08/18
+def bersih():
+     os.system("clear")
 
-clear
-# Variables
-b='\033[1m'
-u='\033[4m'
-bl='\E[30m'
-r='\E[31m'
-g='\E[32m'
-bu='\E[34m'
-m='\E[35m'
-c='\E[36m'
-w='\E[37m'
-endc='\E[0m'
-enda='\033[0m'
-blue='\e[1;34m'
-cyan='\e[1;36m'
-red='\e[1;31m'
+def main():
+    time.sleep(1)
+    bersih()
+    time.sleep(1)
+    print ('\33[1;33m                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            Hello \33[31;1mBangsat \33[0;36m:)  ')
+    time.sleep(1)
+    bersih()
+    print ('\33[32;1m')
+    os.system ('figlet Tools Crack FB')
+    print'\33[36;1m+\33[1;33m===========================================\33[36;1m+'
+    print'\33[36;1mAuthor    \33[31;1m: \33[1;33mKRISTIANA ELI '
+    print'\33[36;1mFacebook    \33[31;1m: \33[1;33mhttps://github.com/eli '
+    print'\33[36;1m+\33[1;33m===========================================\33[36;1m+'
+    print'\33[32;1m++++++++++++  \33[37;1mM E N U \33[32;1m++++++++++++'
+    print'\33[32;1m[\33[37;1m1\33[32;1m] \33[36;1mHack Fb Login Token '
+    print'\33[32;1m[\33[37;1m2\33[32;1m] \33[36;1mHack Fb Login Cokie '
+    print'\33[32;1m[\33[37;1m7\33[32;1m] \33[36;1mUbah Cookies Jadi Token '
+    print'\33[32;1m[\33[31;1m0\33[32;1m] \33[31;1mExit '
+    gans = raw_input ('\33[36;1m==> ')
+    if gans in ['1']:
+        time.sleep(1)
+        os.system('pkg install python2')
+        os.system('pip2 install git')
+        os.system('pip2 install requests')
+        os.system('pip2 install mechanize')
+        os.system ('git clone https://github.com/MantanPrawan/khairulfatihin')
+        bersih()
+	time.sleep(2)
+	print'\33[1;33mBERHASIL DIISNSTALL.......'
+	bersih()
+        os.system('cd khairulfatihin;python2 khairulfatihin.py')
+    if gans in ['2']:
+        time.sleep(1)
+        os.system('pkg update && pkg upgrade')
+        os.system('pkg install python git')
+        os.system('pip install requests bs4 futures')
+        os.system('git clone https://github.com/cyberlombok/cyberlombok')
+        bersih()
+	time.sleep(2)
+        print'\33[1;33mBERHASIL DIISNSTALL.......'
+	bersih()
+        os.system('cd cyberlombok;python cyberlombok.py')
+	sys.exit
+    if gans in ['3']:
+	os.system('pkg update && pkg upgrade')
+	os.system('pkg install python git')
+	os.system('pip install requests')
+	os.system('git clone https://github.com/dz-id/fb_get_token_from_cookie')
+	bersih()
+	time.sleep(2)
+	print'\33[1;33mBERHASIL DIISNSTALL.......'
+        bersih()
+	os.system('cd fb_get_token_from_cookie;python main.py')
+    if gans in ['0']:
+        time.sleep(2)
+        exit()
+    else:
+        time.sleep(1)
+        print ' Pilih Yang Bener Goblok . . .'
+        time.sleep(1)
+        main()
 
-figlet TUAN B4DUT  | lolcat
-
-echo -b "_____________________________________________________________" | lolcat
-echo -b "TYPE      : TOOLS INSTALLER $green " |lolcat
-echo -b "VERSION   : V.3 " | lolcat
-echo -b "TOTALS    : 2 TOOLS " | lolcat
-echo -b "AUTHOR    : MANTAN PERAWAN $green " |lolcat
-echo -b "ASSOCIATE : INDONESIAN TERMUX ASSOCIATION $green " |lolcat
-echo -b "ASSOCIATE : INDONESIAN TERMUX TUTORIAL $green " |lolcat
-echo -b "ASSOCIATE : INDONESIAN DARK TERMUX ASSOCIATE $green " |lolcat
-echo -b "NOTES     : JELAJAHI SETIAP TOOLS DENGAN BIJAK " | lolcat
-echo -b "_____________________________________________________________" | lolcat
-
-sleep 1
-
-###################################################
-# CTRL + C
-###################################################
-trap ctrl_c INT
-ctrl_c() {
-clear
-echo -b $green"[#]> TOOLS YANG KALIAN INSTALL ADA DI DIRECTORY TOOLS GUA " |lolcat
-echo -b $green"[#]> Thanks For Using My Tools" |lolcat
-sleep 1
-echo -b $green"[#]> MANTAN PERAWAN On Your System" |lolcat
-
-echo -b $green"[#]> See you Again SCRIPTKIDDIESðŸ˜Ž :)..." |lolcat
-figlet TUAN B4DUT | lolcat
-sleep 1
-exit
-}
-
-
-lagi=1
-while [ $lagi -lt 6 ];
-do
-
-echo ""
-echo ""
-
-echo -e "######################################" | lolcat
-echo -e "#SILAHKAN TUNGGU DAN LIHAT LIST TOOLS#" | lolcat
-echo -e "######################################" | lolcat
-
-echo ""
-echo -e "============================" | lolcat
-echo -e $b "1. Install Tool DarkFb Token${enda}";
-echo -e "============================" | lolcat
-echo -e $b "2. Install Tool DarkFb Cokies${endcla}";
-echo -e "============================" | lolcat
-echo -e $b "00. Exit${enda}";
-echo -e "============================" | lolcat
-echo -e "â•­â”€[PILIH NOMORNYA]"
-read -p "   â•°â”€root@./MANTAN PERAWAN=" pil;
-
-#Install Tool DarkFb Token
-
-case $pil in
-1) git clone https://github.com/MantanPrawan/khairulfatihin"
-echo -e "${y} cd khairulfatihin"
-echo -e "${y} python2 khairulfatihin.py"
-
-echo
-
-;;
-
-#Install  Tool DarkFb Cokies
-
-2) git clone https://github.com/cyberlombok/cyberlombok"
-echo -e "${y} pkg install git curl python"
-echo -e "${y} pkg install ruby"
-echo -e "${y} pip install mechanize requests bs4 futures"
-echo -e "${y} cd cyberlombok"
-echo -e "${y} python cyberlombok.py"
-
-echo
-
-;;
-
-00) echo "AUTHOR: CYBER LOMBOK" | lolcat
-echo "CYBER LOMBOK ON YOUR SYSTEM" | lolcat
-echo "INDONESIAN TERMUX ASSOCIATION" | lolcat
-echo "SEMUA TOOLS YANG KALIAN INSTALL ADA DI DIRECTORY TOOLS GUA" | lolcat
-figlet LOMBOK CYBER | lolcat
-exit
-;;
-
-*) echo "Sorry, Your choices it's not already [T4T]"
-esac
-done
-done
-
-#[696969696969]
-#[Mantan Prawan]
+main()
